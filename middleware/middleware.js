a1b181c696dd43365819d4593850086232be517e
Senama <sergeenama@gmail.com>
2019-02-02 23:21:20 -0500

    committed

---
+++ middleware/middleware.js
@@ -0,0 +1,92 @@
+const userService = require('../services/user')
+const commentService = require('../services/comment')
+const postService = require('../services/post')
+
+const userAuth = (req,res,next) => {
+    const {user_id} = req.params
+    return userService.read(user_id)  
+    .then((response)=>{
+        const dbToken = response.token
+        if(dbToken === null) {
+            res.json({Error:'User not logged in'})
+            return
+        }
+        
+        const headersToken = req.headers['token']
+        if(headersToken === dbToken) next()
+        
+        else res.json('token incorrect')    
+    },err=>{throw new Error(`user with id: ${id} does not exist`)})
+    .catch(err=>{
+        res.json(err.toString())
+    })
+
+}
+
+const postAuth = (req,res,next)=>{
+    const {post_id} = req.params
+    
+    if(post_id === undefined){
+        if(req.headers['token']) {
+            next()
+        }
+        else res.json({Error:'Need token to put comment'})
+    }
+    else{
+        return postService.readPost(post_id)
+        .then(response=>{
+            console.log(response.author)
+            return userService.read(response.author)
+        })
+        .then((response)=>{
+            console.log(response)
+            const headersToken = req.headers['token']
+            const dbToken = response.token
+            if(headersToken === dbToken)
+                next()
+            else if(dbToken === null) throw new Error('user not logged in')   
+            else res.json('token incorrect')    
+        })
+        .catch(err=>{
+            res.json(err.toString())
+        })
+    }
+}
+
+const commentAuth = (req,res,next) =>{
+    const {comment_id} = req.params
+    if(comment_id === undefined){
+        if(req.headers['token']) {
+            next()
+        }
+        else res.json({Error:'Need token to put comment'})
+
+    }
+    else{
+        return commentService.readCommentWithID(comment_id)
+        .then(response=>{
+            console.log(response.author)
+            return userService.read(response.author)
+        })
+        .then((response)=>{
+            console.log(response)
+            const headersToken = req.headers['token']
+            const dbToken = response.token
+            if(headersToken === dbToken)
+                next()
+            else if(dbToken === null) throw new Error('user not logged in')   
+            else res.json('token incorrect')    
+        })
+        .catch(err=>{
+            res.json(err.toString())
+        })
+    }
+    
+
+}
+
+module.exports = {
+    userAuth,
+    commentAuth,
+    postAuth
+}
